6ec95aafd1e095d032a590611565a4e34dd31b7a
Heshan Abeywickrema <habeywickrema@fb.com>
2021-02-14 14:32:53 -0600

    first commit

---
+++ eos.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+str="$1"
+
+IFS=':'     # colon (:) is set as delimiter
+
+read -ra LINK <<< "$str" # str is read into an array as tokens separated by IFS
+#echo "${LINK[0]}"
+#echo "${LINK[1]}"
+
+IFS=' '     # reset to default value after usage
+
+ssh "${LINK[0]}" << EOF
+  sh int ${LINK[1]} | inc protocol
+  sh int ${LINK[1]} | inc link status
+  sh int ${LINK[1]} | inc error
+  sh lldp neighbor ${LINK[1]}
+  sh int ${LINK[1]}
+  sh int ${LINK[1]}-4 transceiver detail
+  clear counters ${LINK[1]}
+EOF
+
